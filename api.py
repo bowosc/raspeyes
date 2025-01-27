fae60f37c369d576ad5c3b8d8c03b49f8aa12389
Bowman Edebohls <bowman@edebohls.com>
2025-01-26 16:13:43 -0800

    Minimum possible transcription engine

--- api.py
+++ api.py
@@ -1,3 +1,9 @@
-#mmmbmb
-import openai
+# https://github.com/openai/whisper
 
+# Note: whisper apparently doesn't yet work with Python 3.13, I had to downgrade to 3.10 to get it running.
+import whisper
+
+if __name__ == ("__main__"):
+  model = whisper.load_model("tiny")
+  result = model.transcribe("assets/test-transcription.mp3")
+  print(result["text"])
